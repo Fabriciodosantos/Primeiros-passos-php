ae70994461e4cf91e6288888ea1c24542292c294
Fabriciodossantos <fsan4344@gmail.com>
2023-01-13 20:02:02 -0300

    listas e loop em lista

---
+++ Avancando/listas.php
@@ -0,0 +1,10 @@
+<?php
+
+$idadeList = [25 , 36 , 40 , 80 , 90];
+
+$umaIdade = $idadeList [4];
+
+echo $umaIdade;
+
+
+
---
+++ Avancando/loop-lista.php
@@ -0,0 +1,7 @@
+<?php
+
+$idadeList = [25 , 36 , 40 , 80 , 90, 70, 95, 200];
+
+for ($i = 0; $i < count($idadeList); $i++) {
+    echo "# $idadeList[$i] " . PHP_EOL;
+}

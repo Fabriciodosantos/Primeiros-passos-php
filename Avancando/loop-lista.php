39f6c71265c4721863b0e82d745a9ec24b0e56ef
Fabriciodossantos <fsan4344@gmail.com>
2023-01-19 19:13:46 -0300

    .

--- Avancando/loop-lista.php
+++ Avancando/loop-lista.php
@@ -2,6 +2,8 @@
 
 $idadeList = [25 , 36 , 40 , 80 , 90, 70, 95, 200];
 
-for ($i = 0; $i < count($idadeList); $i++) {
+$totalLista = count ($idadeList);
+
+for ($i = 0; $i < $totalLista; $i++) {
     echo "# $idadeList[$i] " . PHP_EOL;
 }
